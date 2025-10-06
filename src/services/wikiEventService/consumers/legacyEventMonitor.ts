4416371e8ba329df219b2a451c07134bde6571a7
PitchforkAssistant <pitchforkassistant@gmail.com>
2025-10-07 00:00:13 +0300

    implement wikiEventMonitor; add start of parsing and routing functionality

--- src/services/wikiEventService/consumers/legacyEventMonitor.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @file This is where we'll parse the older wiki event format, ideally we'll upgrade all versions and drop this eventually.
- */
-
-// Legacy pages: remove, approve, delete, post
-// post
-// const match = revision.reason.match(/Post (t3_[\w\d]+) with goal (\d+)/);
-// remove, approve, delete
-// const match = revision.reason.match(new RegExp(`Dispatch ${actionType} for (t3_[\\w\\d]+)`));
